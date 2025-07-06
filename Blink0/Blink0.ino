6c018c97233774d6a9036105bcb657f31c6e0e5e
sas26031962 <sasx26031962@gmail.com>
2025-07-06 12:46:16 +0300

    Добавление файла программы

--- Blink0/Blink0.ino
+++ Blink0/Blink0.ino
@@ -1,12 +1,14 @@
 #include <Arduino.h>
-#define LED_PIN 2
+
+#define DELAY 350
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT); // GPIO8 - встроенный LED
 }
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
+  delay(DELAY);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+  delay(DELAY);
 }
